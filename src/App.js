bb4ce611d02b6e71775780156513d2f6a284981e
sagarPrajapati <jnvsagarprajapati3224@gmail.com>
2023-08-12 23:11:36 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 // import React from 'react'
 import Hero from './components/Hero'
 // import Table from './components/Table'
-import Demo from './components/Demo'
+// import Demo from './components/Demo'
 // import Login from './components/login'
 
 const App = () => {
   return (
     <>
       <Hero />
-      <Demo />
+      {/* <Demo /> */}
       {/* <Table /> */}
       {/* <Login /> */}
     </>
